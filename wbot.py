2b15a82610f0bf05b770c48e56d6a26159d1d2bf
Akshay Saxena <a42saxen@uwaterloo.ca>
2019-01-01 13:01:37 -0500

    Weather bot

--- wbot.py
+++ wbot.py
@@ -1,9 +1,8 @@
 import requests
 
-api_address = 'https://openweathermap.org/data/2.5/weather?appid=1fdfa3874cc07dedf321967cfd339def&q='
 city = input('City Name: ')
 
-url = api_address + city
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=1fdfa3874cc07dedf321967cfd339def' 
 
 json_data = requests.get(url).json()
 formatted_data = json_data['weather'][0]['main']
